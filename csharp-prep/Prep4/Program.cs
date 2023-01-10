2e7d57dba7890b32c2f0a79ddb8f2538dc9c1e4f
seregirl <agatha>
2023-01-10 03:59:24 +0100

    message

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -7,6 +7,7 @@ class Program
         Console.WriteLine("Hello Prep4 World!");
         List<int> numbers = new List<int>();
         
+        // Please note we could use a do-while loop here instead
         int userNumber = -1;
         while (userNumber != 0)
         {
@@ -15,11 +16,14 @@ class Program
             string userResponse = Console.ReadLine();
             userNumber = int.Parse(userResponse);
             
+            // Only add the number to the list if it is not 0
             if (userNumber != 0)
             {
                 numbers.Add(userNumber);
             }
         }
+
+        // Part 1: Compute the sum
         int sum = 0;
         foreach (int number in numbers)
         {
@@ -29,14 +33,13 @@ class Program
         Console.WriteLine($"The sum is: {sum}");
         float average = ((float)sum) / numbers.Count;
         Console.WriteLine($"The average is: {average}");
-        
         int max = numbers[0];
 
         foreach (int number in numbers)
         {
             if (number > max)
             {
-                
+
                 max = number;
             }
         }
