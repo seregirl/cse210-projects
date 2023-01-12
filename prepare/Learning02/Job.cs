52beefde27843054468eb4e929d8595be98b745b
seregirl <agatha>
2023-01-12 03:30:22 +0100

    Resume and job details

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,15 @@
+using System;
+
+// Responsibilities of job is to Keep track of the company, job title, start year, and end year.
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
